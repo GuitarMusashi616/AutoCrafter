e189928fd0a3ffa2367ac451d5afb53909c7cef6
= <=>
2022-01-24 14:03:12 -0900

    new startup file

--- lib/download.lua
+++ lib/download.lua
@@ -6,11 +6,22 @@ local lzw = require "lib/lualzw"
 local Download = class()
 
 function Download:_init()
-  self.recipes_url = ""
-  self.tags_url = ""
+  self.recipes_url = "https://raw.githubusercontent.com/GuitarMusashi616/AutoCrafter/compresstest/resources/recipes.lzw"
+  self.tags_url = "https://raw.githubusercontent.com/GuitarMusashi616/AutoCrafter/compresstest/resources/tags.lzw"
 end
 
 function Download:get_string(url)
+  local resp, err = http.get(url)
+  if not resp then
+    error(err, 0)
+  end
+  return resp:readAll()
+end
+
+function Download:lzw_json_as_tbl(url)
+  local data_str = self:get_string(url)
+  local json_str = lzw.decompress(data_str)
+  return json.decode(json_str)
 end
 
 function Download:get_recipes()
@@ -18,8 +29,10 @@ function Download:get_recipes()
   -- resp read all
   -- decompress to json
   -- decode to tbl
+  return self:lzw_json_as_tbl(self.recipes_url)
 end
 
 function Download:get_tags()
+  return self:lzw_json_as_tbl(self.tags_url)
 end
 
